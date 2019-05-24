20b05d76ed950bdc281ce1f98b588eda6944944c
Tyler Stein <tyler.stein1@yahoo.ca>
2019-05-24 00:31:40 -0400

    Buildup Point Logic & Animation

--- Assets/ScriptsV2/Behaviours/SupplyPointBehaviour.cs
+++ Assets/ScriptsV2/Behaviours/SupplyPointBehaviour.cs
@@ -16,10 +16,12 @@ namespace AlexaRun.Behaviours
         [SerializeField] private Transform stackPositionRoot = null;
         [SerializeField] private ItemObjectPool itemPool = null;
         [SerializeField] private SupplyPointDefinition definition = null;
+        [SerializeField] private PointAnimationDefinition animationDefinition = null;
+        [SerializeField] private Animator animator = null;
         [SerializeField] private Stack<ItemBehaviour> itemStack = new Stack<ItemBehaviour>();
         [SerializeField] [ReadOnly] private bool isEnabled = true;
-    
-        private float supplyCounter = 0f;
+        [SerializeField] [ReadOnly] private float secondsPerChange = 0f;
+        [SerializeField] [ReadOnly] private float pointTimer = 0f;
 
         public override bool OnInteract(PlayerBehaviour player) {
             if (isEnabled && itemStack.Count > 0 && player.HasStackSpace()) {
@@ -32,24 +34,37 @@ namespace AlexaRun.Behaviours
 
         public override void SetEnabled(bool enabled) {
             isEnabled = enabled;
+            animator.SetInteger("state", enabled ? 1 : 0);
         }
 
         private void Update() {
-            updatePointState(Time.deltaTime);
+            updatePointBehaviour(Time.deltaTime);
         }
 
         private void Awake() {
             if (stackPositionRoot == null) stackPositionRoot = transform;
+            updateSpeed();
             initializeStackContent();
         }
 
-        private void updatePointState(float deltaTime) {
+        private void updateSpeed() {
+            secondsPerChange = 1.0f / definition.baseAccumulationPerSecond;
+            secondsPerChange /= Settings.Persistent.DifficultyScale;
+
+            float animationSpeed = animationDefinition.baseAnimationCycleTime / secondsPerChange;
+
+            animator.SetInteger("state", enabled ? 1 : 0);
+            animator.SetFloat("speed", animationSpeed);
+
+            Debug.Log("SupplyPoint (SupplySpeed = " + secondsPerChange + ") (AnimSpeed = " + animationSpeed + ")", gameObject);
+        }
+
+        private void updatePointBehaviour(float deltaTime) {
             if (isEnabled == false) return;
 
-            float timeTarget = 1.0f / definition.baseAccumulationPerSecond;
-            supplyCounter += deltaTime;
-            while (supplyCounter >= timeTarget) {
-                supplyCounter -= timeTarget;
+            pointTimer += deltaTime;
+            while (pointTimer >= secondsPerChange) {
+                pointTimer -= secondsPerChange;
                 pushNewItemToStack();
             }
         }
@@ -69,7 +84,11 @@ namespace AlexaRun.Behaviours
         }
 
         private void pushNewItemToStack() {
-            if (itemStack.Count >= definition.maxStackItemCount) return;
+            if (itemStack.Count >= definition.maxStackItemCount) {
+                animator.SetInteger("state", 0);
+                return;
+            }
+            animator.SetInteger("state", 1);
             ItemBehaviour newItem;
             itemPool.SpawnFromPool(out newItem, definition.suppliedItem.itemName);
             if (newItem == null) throw new UnityException(string.Format("Failed to initialize supply stack {0} with item {1}!", gameObject.name, definition.suppliedItem.name));
